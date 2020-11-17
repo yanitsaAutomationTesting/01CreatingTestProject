cf1c9be143766afdb846772804a12614d6e7f417
yanitsaAutomationTesting <y.handzhieva@gmail.com>
2020-11-17 15:20:43 +0200

    Steps Definitions are separated, Hooks for before and after scenario are introduced

---
+++ CreaditCards.UITests/StepDefinitions/AboutPageSteps.cs
@@ -0,0 +1,30 @@
+﻿using CreaditCards.UITests.PageObjectModels;
+using TechTalk.SpecFlow;
+
+namespace CreaditCards.UITests.StepDefinitions
+{
+    [Binding]
+    class AboutPageSteps
+    {
+        private StepDefinitionsContextInjection _context;
+        AboutPageSteps(StepDefinitionsContextInjection context)
+        {
+            _context = context;
+        }
+
+        [Given(@"I go to the About page")]
+        public void GivenIGoToTheAboutPage()
+        {
+            _context.AboutPage = new AboutPage(_context._driver);
+            _context.AboutPage.NavigateTo(_context._driver);
+        }
+
+        [When(@"I go back from the About Page")]
+        public void WhenIGoBack()
+        {
+            _context.AboutPage.GoBackwards();
+        }
+
+
+    }
+}
---
+++ CreaditCards.UITests/StepDefinitions/ApplicationCompletePageSteps.cs
@@ -0,0 +1,30 @@
+﻿using TechTalk.SpecFlow;
+
+namespace CreaditCards.UITests.StepDefinitions
+{
+    [Binding]
+    class ApplicationCompletePageSteps
+    {
+        private StepDefinitionsContextInjection _context;
+        ApplicationCompletePageSteps(StepDefinitionsContextInjection context)
+        {
+            _context = context;
+        }
+
+        [When(@"I am on the Application Complete Page")]
+        public void WhenIAmOnTheApplicationCompletePage()
+        {
+            _context.ApplicationCompletePage.EnsurePageLoaded();
+        }
+
+        [Then(@"the full name is (.*)")]
+        public void ThenTheFullNameIsSarahParker(string fullName)
+        {
+            _context.ApplicationCompletePage.verifyFullName(fullName);
+        }
+
+
+
+
+    }
+}
